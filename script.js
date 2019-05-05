6b6c1488ca4ca99de15bd3daff2b9e4e9fe7b6ee
ghett <aivanicky@gmail.com>
2019-05-05 05:18:09 +0300

    commented

--- script.js
+++ script.js
@@ -6,12 +6,10 @@ Number.prototype.round = function(places) {
 arr_merge = (arr0, arr1) => arr0.map((item, i) => (item + arr1[i]).round(2));
 //to calculate current vector length
 arr_magnitude = (arr) => (Math.hypot(...arr));
-
 function fragmentator (source, output_storage) {
 	for (key in source) {
 		//initiate subobject for each path
 		output_storage[key] = {};
-
 		//-1 FROM END
 		var tmp_arr = source[key].slice();
 		while (tmp_arr.length>1) {
@@ -20,7 +18,6 @@ function fragmentator (source, output_storage) {
 			tmp_arr.forEach(function(item,index){
 				//path dasharray/dashoffset is a sum of all contained vectors magnitude
 				tmp_dash = (tmp_dash+arr_magnitude(item)).round(0);
-
 				//path data combination
 				if (index==0) {
 					tmp_str='m';
@@ -33,13 +30,11 @@ function fragmentator (source, output_storage) {
 			output_storage[key][tmp_str]=tmp_dash;
 			tmp_arr.pop();
 		};
-
 		//-1 FROM START
 		tmp_arr = source[key].slice();
 		while (tmp_arr.length>2) {
 			var tmp_str = '';
 			var tmp_dash = 0;
-			//
 			tmp_arr[1] = arr_merge(tmp_arr[0],tmp_arr[1]);
 			tmp_arr.shift();
 			tmp_arr.forEach(function(item,index){
@@ -50,13 +45,11 @@ function fragmentator (source, output_storage) {
 			});
 			output_storage[key][tmp_str]=tmp_dash;
 		};
-
 		//-1 TO MIDDLE
 		tmp_arr = source[key].slice();
 		tmp_arr[1] = arr_merge(tmp_arr[0],tmp_arr[1]);
 		tmp_arr.shift();
 		tmp_arr.pop();
-		
 		while (tmp_arr.length>2) {
 			var tmp_str = '';
 			var tmp_dash = 0;
